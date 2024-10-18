e52a283960184acad3c1d6719f233a5bb0c54960
artur-vinicius <artur.t@escolar.ifrn.edu.br>
2024-10-18 15:18:25 -0300

    Adding route configs

--- login/src/App.js
+++ login/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/login';
+import Register from './components/register';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} /> {/* Página inicial será o login */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<h1>404 - Página não encontrada</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
