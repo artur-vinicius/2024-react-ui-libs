d458c5a577e12d5798bb4f0503adbd34c42f5147
artur-vinicius <artur.t@escolar.ifrn.edu.br>
2024-10-18 15:24:31 -0300

    Updating title color

--- login/src/components/login.js
+++ login/src/components/login.js
@@ -32,7 +32,7 @@ const Login = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center text-purple-600">Login</h2>
 
         {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
 
--- login/src/components/register.js
+++ login/src/components/register.js
@@ -32,7 +32,7 @@ const Register = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Cadastro</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center text-purple-600">Cadastro</h2>
 
         {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
 
